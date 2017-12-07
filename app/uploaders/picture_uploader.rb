4a24700e5b1ec2c7dd614ca7b063128c2ac35d8a
Nguyen Van Trung <nguyenvantrungnct@gmail.com>
2017-12-07 18:50:18 +0700

    Main code model tour and Set permission

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,17 @@
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [200, 200]
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end
