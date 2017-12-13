9d0e47e9576a555c2dd707c94d336aef530cdf31
NuongNguyen1996 <nuong14pt@gmail.com>
2017-12-13 18:30:52 +0700

    book tour successfull

--- app/models/tourdetail.rb
+++ app/models/tourdetail.rb
@@ -1,4 +1,9 @@
 class Tourdetail < ApplicationRecord
   belongs_to :tour
   belongs_to :user
+
+  validates :tour_id, presence: true
+  validates :user_id, presence: true
+  validates :num_of_pass, presence: true, format: {with: /\A\d+\z/}
+  validates :total_price, presence: true, format: {with: /\A\d+\z/}
 end
